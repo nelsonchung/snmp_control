e8c2fa7af8228e081d4be388058f8376a92b6bce
nelsonchung <chihchun.chung@gmail.com>
2016-06-20 13:48:03 +0800

    Rollback 0_setenv.sh to 12f72e07373e20e98529af977024416d4cb0c64a

--- 0_setenv.sh
+++ 0_setenv.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#for 6643
-#export SNMP_IP="172.16.90.179"
-export SNMP_IP="172.16.90.81"
-#export SNMP_IP="172.16.90.26"
-#export SNMP_IP="172.16.180.7"
-#for 6541 
-#export SNMP_IP="172.16.90.26"
-#export SNMP_IP="172.16.70.41"
-#export SNMP_IP="172.16.21.80"
-#for 6541 bill
-#export SNMP_IP="172.16.90.36"
+export SNMP_IP="172.16.90.46"
 #export SNMP_IP="172.16.180.2"
 export SNMP_PRIV="-c private -v2c "
