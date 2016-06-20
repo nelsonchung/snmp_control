737537baab906c4210749dee6d4dc9b69845a1e3
nelsonchung <chihchun.chung@gmail.com>
2016-06-20 13:37:00 +0800

    Add more firmware version.

--- 5_upgrade.sh
+++ 5_upgrade.sh
@@ -4,6 +4,8 @@ echo "支援以下版本升級"
 count=1
 echo "${count}. CH6643-3.5.1.12b-NOSH-TW.NNEMN.p7"
 count=`expr $count + 1`
+echo "${count}. CH6643-3.5.1.12b-SH-TW.NNEMN.p7"
+count=`expr $count + 1`
 echo "${count}. CH6643-3.7.0.1j-SH-TW.NNEMN.p7"
 count=`expr $count + 1`
 echo "${count}. CH6643-3.7.0.1k-SH-TW.NNEMN.p7"
@@ -17,6 +19,14 @@ count=`expr $count + 1`
 echo "${count}. CH6643-3.7.0.1q-SH-TW.NNEMN.p7"
 count=`expr $count + 1`
 echo "${count}. CH6643-3.7.0.1r-SH-TW.NNEMN.p7"
+count=`expr $count + 1`
+echo "${count}. CH6643-3.7.0.1u-SH-TW.NNEMN.p7"
+count=`expr $count + 1`
+echo "${count}. CH6643-3.7.0.2-SH-TW.NNEMN.p7"
+count=`expr $count + 1`
+echo "${count}. CH6643-3.7.0.2f-SH-TW.NNEMN.p7"
+count=`expr $count + 1`
+#count=`expr $count + 1`
 #count=`expr $count + 1`
 #echo "${count}. "
 read option
@@ -32,25 +42,37 @@ case $option in
     FW_VER="CH6643-3.5.1.12b-NOSH-TW.NNEMN.p7"
     ;;
     "2")
-    FW_VER="CH6643-3.7.0.1j-SH-TW.NNEMN.p7"
+    FW_VER="CH6643-3.5.1.12b-SH-TW.NNEMN.p7"
     ;;
     "3")
-    FW_VER="CH6643-3.7.0.1k-SH-TW.NNEMN.p7"
+    FW_VER="CH6643-3.7.0.1j-SH-TW.NNEMN.p7"
     ;;
     "4")
-    FW_VER="CH6643-3.7.0.1m-SH-TW.NNEMN.p7"
+    FW_VER="CH6643-3.7.0.1k-SH-TW.NNEMN.p7"
     ;;
     "5")
-    FW_VER="CH6643-3.7.0.1n-SH-TW.NNEMN.p7"
+    FW_VER="CH6643-3.7.0.1m-SH-TW.NNEMN.p7"
     ;;
     "6")
-    FW_VER="CH6643-3.7.0.1p-SH-TW.NNEMN.p7"
+    FW_VER="CH6643-3.7.0.1n-SH-TW.NNEMN.p7"
     ;;
     "7")
-    FW_VER="CH6643-3.7.0.1q-SH-TW.NNEMN.p7"
+    FW_VER="CH6643-3.7.0.1p-SH-TW.NNEMN.p7"
     ;;
     "8")
+    FW_VER="CH6643-3.7.0.1q-SH-TW.NNEMN.p7"
+    ;;
+    "9")
     FW_VER="CH6643-3.7.0.1r-SH-TW.NNEMN.p7"
+    ;;
+    "10")
+    FW_VER="CH6643-3.7.0.1u-SH-TW.NNEMN.p7"
+    ;;
+    "11")
+    FW_VER="CH6643-3.7.0.2-SH-TW.NNEMN.p7"
+    ;;
+    "12")
+    FW_VER="CH6643-3.7.0.2f-SH-TW.NNEMN.p7"
     ;;
     *)
         echo  "Wrong value"
